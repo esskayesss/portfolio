ac716ae69996d170c94e32d16028b2f6a7fc4298
esskayesss <imesskayesss@protonmail.com>
2024-12-03 01:45:00 +0530

    minor adjustments, fixed 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,10 +3,13 @@ import Link from "next/link";
 
 export default async function NotFound() {
   return (
-    <div className={`fixed top-0 left-0 w-screen h-screen flex flex-col gap-12 flex-grow justify-center items-center`}>
+    <div className={`fixed p-8 top-0 left-0 w-screen h-screen flex flex-col gap-12 flex-grow justify-center items-center`}>
       <div className="flex flex-col gap-2 text-fg items-center">
-        <span className={`text-6xl font-proto`}>404 | A+ for Effort</span>
-        <span className={`text-lg font-mono`}>The page exists in your heart. Just not on this server.</span>
+        <div className={`font-proto flex gap-4`}>
+          <span className={`text-5xl text-accent-fg`}>404</span>
+          <span className={`text-lg`}>A+ For effort</span>
+        </div>
+        <span className={`text-center text-dim-fg  font-mono`}>The page exists in your heart. Just not on this server.</span>
       </div>
       <div className="flex gap-12 items-center">
         <Link href='/' className="btn">Back to Reality</Link>
