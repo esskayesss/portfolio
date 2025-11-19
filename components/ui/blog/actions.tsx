7f2a0bc33be1ebdd5d08349c4299ddce81d6c5f6
esskayesss <imesskayesss@protonmail.com>
2025-11-19 15:49:04 +0530

    init podcast

--- components/ui/blog/actions.tsx
+++ components/ui/blog/actions.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   PiChatTeardropDots, PiCoin, PiLink,
-  PiWaveformDuotone
 } from "react-icons/pi";
 import Link from "next/link";
 import {TOCButton} from "@/components/ui/blog/toc";
@@ -9,6 +8,7 @@ import {fetchTOC} from "@/app/actions";
 import {CopyToClipboard} from "@/components/ui/clipboard-copy";
 import {FormatButton} from "@/components/ui/blog/reading";
 import {notFound} from "next/navigation";
+import { WavePlayer } from "../podcast/blog-wave";
 
 export const BlogActions: React.FC<{ slug: string }> = async ({slug}) => {
   const toc = await fetchTOC(slug);
@@ -21,7 +21,7 @@ export const BlogActions: React.FC<{ slug: string }> = async ({slug}) => {
       className={`print:hidden fixed bottom-0 h-14 bg-bg w-screen left-0 flex justify-between px-12 py-12 border-t border-ghost items-center z-10`}>
       <div className="relative flex justify-between w-full max-w-6xl mx-auto">
         <div className="left flex items-center gap-8 *:text-3xl *:cursor-pointer">
-          <PiWaveformDuotone className={`disabled`}/>
+          <WavePlayer slug={slug} />
           <Link href={`https://x.com/search?q="https://esskayesss.dev/blog/${slug}"`} target={`_blank`}>
             <PiChatTeardropDots/>
           </Link>
---
+++ components/ui/icons/play/index.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function PlayArrow(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M9 17.192V6.808L17.154 12z"></path></svg>
+  )
+}
